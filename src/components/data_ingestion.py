234705fcd83b91fb243e255e187211a565249373
Brymahh <eseoghenabraimah@gmail.com>
2025-01-24 23:15:02 +0100

    data-transII

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -54,7 +54,7 @@ if __name__=='__main__':
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
 
